e641cb9e47e6a6bfb094818b342fd71cbeb69ce4
Roshankumarjha123 <roshanjha8182867@gmail.com>
2025-06-22 14:12:04 +0530

    React files

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1> Chai aur react with basic react </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+ 
+
+  return (
+    <h1> Hello world with vite | Rk </h1>
+  )
+}
+
+export default App
